7ccd678b1350687fbe16d67d9e2b242879dbb903
Luna Sparklen <luna_@peridot.massacres.fun>
2017-08-05 12:49:25 +1000

    Initial commit for simple helper functions

---
+++ helpers.go
@@ -0,0 +1,19 @@
+package pantsu-api
+
+import (
+  "log"
+)
+
+// shorthand wrapper function for non fatal errors
+func logError(err error) {
+  if err != nil {
+    log.Println(err.Error())
+  }
+}
+
+// shorthand wrapper function for fatal errors
+func logErrorFatal(err error) {
+  if err != nil {
+    log.Fatalln(err.Error())
+  }
+}
